1ac4c5a6eac9d2249ac84aefc108b908a8657e4c
StevenGreenup <greenup.steven@gmail.com>
2016-06-28 15:43:25 -0500

    added a test for creating books

---
+++ test/features/can_addnewbook_add_book_to_root_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class CanAddnewbookAddBookToRootTest < Capybara::Rails::TestCase
+
+  setup do
+    Author.create! first_name: "Mark", last_name: "Twain", Bio:"He is an author"
+  end
+
+
+  test "Can add new book add book" do
+    visit root_path
+    click_link "Add New Book"
+    fill_in "Title", with: "Huckleberry Finn"
+    fill_in "Photo url", with: "http://cdn3.volusion.com/jtoq7.b7owf/v/vspfiles/photos/ADVENTURES_OF_HUCKLEBERRY_FINN-2.jpg"
+    fill_in "Price in cents", with: "1000"
+    select 'Mark Twain', from: 'Author'
+    click_link "Create Book"
+
+  end
+end
